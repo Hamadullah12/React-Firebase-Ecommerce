d2118c1afe88c9f5135d1f14a103c520b57f7e67
Hamadullah12 <hamadfms05@gmail.com>
2025-02-24 11:51:50 +0800

    problems occuredd

--- React-Firebase-Ecommerce/src/App.jsx
+++ React-Firebase-Ecommerce/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return <Button variant="contained">Hello world for me </Button>;
 }
 
 export default App;
